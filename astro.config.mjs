2190e9cb23d2a6dfe2bf9b72ef96b5e880397272
Denis Zaitsev <zaytsevdi@gmail.com>
2023-03-05 23:18:52 +0200

    pocketbase

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
-// https://astro.build/config
+/* eslint-disable @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment */
+// import partytown from "@astrojs/partytown"
 import solidJs from "@astrojs/solid-js"
+import tailwind from "@astrojs/tailwind"
 import {defineConfig} from "astro/config"
 
-// https://astro.build/config
+
+// const {NODE_ENV} = process.env
+
 export default defineConfig({
-  integrations: [solidJs()],
+  integrations: [
+    solidJs(),
+    tailwind(),
+    // TODO: partytown is not working
+    // partytown({
+    //   config: {debug: NODE_ENV === "development"},
+    // }),
+  ],
 })
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,16 +1,17 @@
-import {createSignal, JSXElement, ParentProps} from "solid-js"
+import {Component, createSignal, ParentProps} from "solid-js"
 import "./Counter.css"
 
 
-const Counter = (props: ParentProps): JSXElement => {
-  const [count, setCount] = createSignal(0);
+const Counter: Component = (props: ParentProps) => {
+  const [count, setCount] = createSignal(0)
   const add = (): number => setCount(count() + 1)
   const subtract = (): number => setCount(count() - 1)
 
   return (
     <>
-      <div class="unknown" id={"2"} />
-      <div class="counter" id="1">
+      <div
+        class="counter"
+        id="1">
         <button onClick={subtract}>-</button>
         <pre>{count()}</pre>
         <button onClick={add}>+</button>
